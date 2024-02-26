1fdf6d418860e354fb1f29ad991cbc3f0e46f397
henriquepontees <104809502+henriquepontees@users.noreply.github.com>
2024-02-26 11:33:18 -0300

    Update settings.py

--- projetochaconv1/settings.py
+++ projetochaconv1/settings.py
@@ -19,15 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l#@v76@zhn&h*$vx6y6d=r04)@urcud(k#7q7#r^u8sw#jrslz'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['henriquechaconturismo.pythonanywhere.com']
-
-
 # Application definition
 
 INSTALLED_APPS = [
